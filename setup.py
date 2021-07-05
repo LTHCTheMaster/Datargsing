dedb66abf51b2c74c4fb04192e42f337fdce5a7b
LTHCTheMaster <mistermega52@gmail.com>
2021-07-05 17:36:55 +0200

    0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='Datargsing',
     packages=find_packages(include=['datargsing'], exclude=['tests']),
-    version='0.1.0',
+    version='0.1.1',
     description='A Data management and manipulation library',
     author='LTHC The Master (CHOSSY Lucas)',
     license='MIT',
