bf3bb9ddc65686777e6a74caaa2d6162c37287aa
e_ψ_lon <79647119+e-psi-lon@users.noreply.github.com>
2023-01-01 14:51:52 +0100

    Update _version.py

--- datargsing/_version.py
+++ datargsing/_version.py
@@ -1,18 +1,2 @@
 from importlib.metadata import PackageNotFoundError, version
-try:
-    __version__ = version("datargsing")
-except PackageNotFoundError:
-    # Package is not installed
-    try:
-        from setuptools_scm import get_version  # type: ignore[import]
-
-        __version__ = get_version()
-    except ImportError:
-        # setuptools_scm is not installed
-        __version__ = "0.0.0"
-        warnings.warn(
-            "Package is not installed, and setuptools_scm is not installed. "
-            f"As a fallback, {__name__}.__version__ will be set to {__version__}",
-            RuntimeWarning,
-            stacklevel=2,
-        )
+__version__ = version("datargsing")
